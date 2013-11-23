8c00e93ee6d34192a80be5a5ecbec6d941270584
Apprentice <mail@danbender.net>
2013-11-23 15:01:04 -0800

    rails new

---
+++ README.md
@@ -0,0 +1,4 @@
+A web-app for Dev Bootcamp alumni to share and vote on programming learning resources.
+
+Kenneth Yu
+Dan Bender
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodingResourcesOverflow::Application.config.secret_token = '8cb5168cebabe2293de5cdc4926b1e5d5e636828f82d256e28a319d88b28d26e6f5374055a62028cd44b343bf82fefcfa07be9c7ad60d43294bcc793d7dd036d'
