3693e46c5bf978b94570b9ff70e3dda20b297561
Apprentice <kenyu@ucdavis.edu>
2013-11-24 00:39:35 -0800

    dan/ken: fix bug in rspec test - all tests pass!

--- spec/features/guest_can_upvote_source_spec.rb
+++ spec/features/guest_can_upvote_source_spec.rb
@@ -5,13 +5,14 @@ feature 'upvote of a source' do
     @test_source = Source.new
     @test_source.title = "Javascript is awesome"
     @test_source.url = "http://www.javascript.com"
+    @test_source.upvote_count = 0
     @test_source.save
   end
 
   context 'guest can upvote a source' do
-    it "allows guests to upvote a source" do
+    it "allows guests to upvote a source", :js => true do
       visit '/'
-      find('.upvote_count').click
+      click_button('Up!')
       page.should have_content(1)
     end
   end
