171f85a401948c7788a57dfb4118b12b33e0a4f9
Apprentice <kenyu@ucdavis.edu>
2013-11-23 16:50:00 -0800

    sources#index with link

--- spec/features/guest_may_post_resources_spec.rb
+++ spec/features/guest_may_post_resources_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-feature 'guest can post a resource' do
+feature 'guest can post a source' do
   it 'displays a form' do
     visit ('/')
-    click_button('Add a Resource')
+    click_link('Add a source')
     fill_in('title', :with => 'Node Life')
     fill_in('url', :with => 'http://www.nodelife.com')
   end
