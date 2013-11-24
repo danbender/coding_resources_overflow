9f7b5ea5f022fa0817d2f87b1ad90d703ce3f140
Apprentice <kenyu@ucdavis.edu>
2013-11-24 00:19:39 -0800

    ken/dan: * Guest can upvote sources * Upvote is updated in database * Ajaxified the upvote

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -15,4 +15,12 @@ class SourcesController < ApplicationController
     redirect_to "/"
   end
 
+  def upvote
+    source = Source.find(params[:id].to_i)
+    source.upvote_count += 1
+    source.save
+    # redirect_to "/"
+    render :json => {source_id: source.id, current_count: source.upvote_count}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 CodingResourcesOverflow::Application.routes.draw do
   root to: 'sources#index'
   resources :sources, only: [ :index, :new, :create ]
+
+  put 'sources/:id', to: 'sources#upvote', as: :source
 end
