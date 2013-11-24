f0b8fa5d2e77a073c9288adf49da9d1e123073dd
Apprentice <kenyu@ucdavis.edu>
2013-11-23 20:52:48 -0800

    ken/dan: * wrote test for sources upvote count * wrote migration to add column upvote_count to sources schema

---
+++ spec/features/guest_can_upvote_source_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+feature 'upvote of a source' do
+  before(:each) do
+    @test_source = Source.new
+    @test_source.title = "Javascript is awesome"
+    @test_source.url = "www.javascript.com"
+    @test_source.save
+  end
+
+  context 'guest can upvote a source' do
+    it "allows guests to upvote a source" do
+      visit '/'
+      find('.upvote-button').click
+      page.should have_content(1)
+    end
+  end
+end
