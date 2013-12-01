dbbca7587fcdb94d1b13a6515396b8ed791e591c
Dan Bender <mail@danbender.net>
2013-12-01 15:29:32 -0800

    dan/ken: remove comments and whitespace

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,11 @@
 class CommentsController < ApplicationController
 
   def create
-    # debugger
     source = Source.find(params[:source_id])
     @comment = source.comments.build(comment_params)
     if @comment.save
-      p "YAY IT WORKED"
       render :json => render_to_string(:partial => 'new_comment', :locals => {:comment => @comment}).to_json
     else
-      "FUCK, EVERYTHING IS FUCKED"
       flash[:error] = 'Comment could not be saved. Please try again.'
       redirect_to source_path(source)
     end
