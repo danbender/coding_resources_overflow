f9044bc96634665d45798251e79484a5bb604ac4
Dan Bender <mail@danbender.net>
2013-11-30 21:42:33 -0800

    ken/dan: * use factory girl to DRY up specs * added strong_params gem * use strong params in sources controller

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -8,15 +8,21 @@ class SourcesController < ApplicationController
   end
 
   def create
-    @source = Source.new
-    @source.title = params[:source][:title]
-    @source.url = params[:source][:url]
-    @source.save
-    redirect_to root_path
+    @source = Source.new(source_params)
+    params = {:source => {url: '', title: ''}}
+    if @source.save
+      redirect_to root_path
+    else
+      flash[:error] = 'You fucked up, son!'
+    end
+    # @source.title = params[:source][:title]
+    # @source.url = params[:source][:url]
+    # @source.save
+    # redirect_to root_path
   end
 
   def upvote
-    source = Source.find(params[:id].to_i)
+    source = Source.find(params[:id])
     source.upvote_count += 1
     source.save
     @sorted_sources = Source.order('upvote_count DESC')
@@ -24,22 +30,26 @@ class SourcesController < ApplicationController
   end
 
   def destroy
-    source_to_delete = Source.find(params[:id].to_i)
+    source_to_delete = Source.find(params[:id])
     source_to_delete.destroy
     render :json => {source_id: source_to_delete.id}
   end
 
   def edit
-    @source_to_edit = Source.find(params[:id].to_i)
+    @source_to_edit = Source.find(params[:id])
     render :partial => 'edit', :locals => {:source_to_edit => @source_to_edit}
   end
 
   def update
-    @source_to_edit = Source.find(params[:id].to_i)
+    @source_to_edit = Source.find(params[:id])
     @source_to_edit.title = params[:source][:title]
     @source_to_edit.url = params[:source][:url]
     @source_to_edit.save
     redirect_to root_path
   end
 
+protected
+  def source_params
+    params.require(:source).permit(:url, :title)
+  end
 end
--- spec/features/guest_can_add_comments_spec.rb
+++ spec/features/guest_can_add_comments_spec.rb
@@ -1,19 +1,16 @@
 # require 'spec_helper'
 
 # feature 'add a comment' do
-#   before(:each) do
-#     @test_source = Source.new
-#     @test_source.title = "Javascript is awesome"
-#     @test_source.url = "http://www.javascript.com"
-#     @test_source.save
-#   end
+# before(:each) do
+#   @test_source = create(:source)
+# end
 
-#   context 'guest can add a comment to an existing source' do
-#     it "allows guests to delete a source", :js => true do
-#       visit '/'
-#       find('.comment-button').click
-#       fill_in('comment-body')
-#       click_button('Add Comment')
-#     end
+# context 'guest can add a comment to an existing source' do
+#   it "allows guests to delete a source", :js => true do
+#     visit '/'
+#     find('.comment-button').click
+#     fill_in('comment-body')
+#     click_button('Add Comment')
 #   end
+# end
 # end
