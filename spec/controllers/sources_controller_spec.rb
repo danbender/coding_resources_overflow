3e2332db953521ec6f15d5b5bd792625faaef73d
Apprentice <kenyu@ucdavis.edu>
2013-12-12 17:52:42 -0800

    dan/ken: fix test in sources_controller_spec

--- spec/controllers/sources_controller_spec.rb
+++ spec/controllers/sources_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe SourcesController do
   let(:params){
-    {source: { title: "Hi, I am a title", url: "http://www.google.com" }}
+    {source: { title: "Hi, I am a title", url: "http://www.google.com", tag_list: "You shit is weak" }}
   }
 
   describe "#create" do
