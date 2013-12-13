54b289c4ef923e17ffb54eb96c29b907a4f65ada
Apprentice <kenyu@ucdavis.edu>
2013-12-12 16:02:51 -0800

    ken/dan: add acts_as_taggable to Source model * made tag_list attr_accessible

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,7 +1,9 @@
 class Source < ActiveRecord::Base
-  attr_accessible :title, :url
+  attr_accessible :title, :url, :tag_list
   validates :title, presence: true, uniqueness: true
   validates :url, presence: true
+
+  act_as_taggable
 
   has_many :comments
 end
